6c731ae6c44577f36face261059c8744ec2a4473
Phil Mills <pmills@newrelic.com>
2015-07-08 09:00:20 -0700

    Testing different db calls for BigList kata

--- app/controllers/big_list_controller.rb
+++ app/controllers/big_list_controller.rb
@@ -1,6 +1,8 @@
 class BigListController < ApplicationController
   def index
-    @all_sites = WebSite.all.to_a
-    @first_site = @all_sites.first
+#    @all_sites = WebSite.all.to_a
+	@all_sites = WebSite.count
+#    @first_site = @all_sites.first
+	@first_site = WebSite.limit(1)
   end
 end
