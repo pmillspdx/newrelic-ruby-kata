61ed42482534488f501ba54a55f222201e3f97bc
Phil Mills <pmills@newrelic.com>
2015-07-08 09:26:57 -0700

    Checking in fix for the Big List kata

--- app/controllers/big_list_controller.rb
+++ app/controllers/big_list_controller.rb
@@ -1,8 +1,6 @@
 class BigListController < ApplicationController
   def index
-#    @all_sites = WebSite.all.to_a
 	@all_sites = WebSite.count
-#    @first_site = @all_sites.first
 	@first_site = WebSite.take
   end
 end
