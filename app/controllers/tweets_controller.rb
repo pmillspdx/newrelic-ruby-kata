543e946c34dfb192289bb7bd2eb172b1ef1c3091
Phil Mills <pmills@newrelic.com>
2015-07-09 09:29:19 -0700

    Removing .invisible span tags in an effor to reduce the amount of DOM

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,7 @@ class TweetsController < ApplicationController
     response = HTTParty.get('http://twitter.com/newrelic')
     
     parsed_data = Nokogiri::HTML.parse response.body
+    parsed_data.css(".invisible").remove
     tweetNodes = parsed_data.css(".js-tweet-text")
     @nodes = tweetNodes.collect do |node|
       node.inner_html
